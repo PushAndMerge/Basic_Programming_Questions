09f049ef389a887975ced5215f26a8539cf8dd8a
Zahid Mehmood <31436393+PushAndMerge@users.noreply.github.com>
2025-07-15 21:31:46 +0500

    Add files via upload

---
+++ 08_odd_numbers_in_an_array.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	// Odd numbers in an Array
+	int arr[5]={7,4,8,12,-5};
+	for(int i=0;i<5;i++){
+		if(arr[i]%2==1 || arr[i]%2 == -1){
+			cout<<arr[i]<<" ";
+		}
+	}
+}
